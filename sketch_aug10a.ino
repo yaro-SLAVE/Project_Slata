12946ff30a054b1e02941f74b8cacf422e6332af
yaro-SLAVE <85624526+yaro-SLAVE@users.noreply.github.com>
2021-08-12 18:04:18 +0800

    Add files via upload

---
+++ sketch_aug10a.ino
@@ -0,0 +1,49 @@
+#include <SPI.h>
+
+#include "nRF24L01.h"
+
+#include "RF24.h"
+
+
+#include <LCD_1602_RUS.h> 
+#include <Wire.h>
+
+LCD_1602_RUS lcd(0x22, 16, 2);
+
+
+//create an RF24 object
+RF24 radio(9, 10);  // CE, CSN
+//address through which two modules communicate.
+const byte address[6] = "00001";
+void setup()
+{
+  while (!Serial);
+    Serial.begin(9600);
+  
+  radio.begin();
+  lcd.init();
+  lcd.backlight();
+  
+  //set the address
+  radio.openReadingPipe(0, address);
+  
+  //Set module as receiver
+  radio.startListening();
+}
+void loop()
+{
+  //Read the data if available in buffer
+  if (radio.available())
+  {
+    lcd.clear();
+    delay(1000);
+    lcd.setCursor(0,0);
+    char text[32] = {0};
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+    lcd.print(text);
+    delay(5000);
+    lcd.clear();
+  }
+  lcd.clear();
+}
---
+++ sketch_aug11a.ino
@@ -0,0 +1,41 @@
+#include <LCD_1602_RUS.h> 
+#include <Wire.h>
+
+LCD_1602_RUS lcd(0x22, 16, 2);
+//Include Libraries
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+//create an RF24 object
+RF24 radio(9, 10);  // CE, CSN
+//address through which two modules communicate.
+const byte address[6] = "00001";
+void setup()
+{
+   Serial.begin(115200);
+lcd.init();
+  lcd.backlight();
+  radio.begin();
+  
+  //set the address
+  radio.openWritingPipe(address);
+  
+  //Set module as transmitter
+  radio.stopListening();
+
+}
+void loop()
+{
+  //Send message to receiver
+
+    if(Serial.available()>0){ 
+       char text = Serial.read();// если в последовательном порту есть данные для чтения, то ...
+                       // устанавливаем курсор в позицию: столбец 15, строка 1
+  radio.write(&text, sizeof(text));
+ 
+  lcd.print(text) ;
+    }
+    delay(2000);
+    
+
+}
